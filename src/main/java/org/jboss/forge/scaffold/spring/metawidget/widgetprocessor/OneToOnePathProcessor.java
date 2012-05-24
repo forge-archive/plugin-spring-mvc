3779afae2544c533f664ccf031c065639cef8270
Ryan Bradley <rbradley@redhat.com>
2012-05-24 15:03:53 -0400

    Add a OneToOnePathProcessor to override the default PathProcessor in the case of @OneToOne relationships.

---
+++ src/main/java/org/jboss/forge/scaffold/spring/metawidget/widgetprocessor/OneToOnePathProcessor.java
@@ -0,0 +1,32 @@
+package org.jboss.forge.scaffold.spring.metawidget.widgetprocessor;
+
+import static org.metawidget.inspector.InspectionResultConstants.*;
+import static org.jboss.forge.scaffold.spring.metawidget.inspector.ForgeInspectionResultConstants.*;
+
+import java.util.Map;
+
+import org.metawidget.statically.StaticXmlWidget;
+import org.metawidget.statically.jsp.StaticJspMetawidget;
+import org.metawidget.statically.spring.widgetprocessor.PathProcessor;
+
+public class OneToOnePathProcessor extends PathProcessor
+{
+    //
+    // Public methods
+    //
+
+    @Override
+    public StaticXmlWidget processWidget( StaticXmlWidget widget, String elementName, Map<String, String> attributes, StaticJspMetawidget metawidget )
+    {
+        super.processWidget(widget, elementName, attributes, metawidget);
+
+        // Only override the processed widget if there is a ONE_TO_ONE relationship
+
+        if (TRUE.equals( attributes.get(ONE_TO_ONE)))
+        {
+            widget.putAttribute("path", attributes.get(NAME) + ".id");
+        }
+
+        return widget;
+    }
+}
