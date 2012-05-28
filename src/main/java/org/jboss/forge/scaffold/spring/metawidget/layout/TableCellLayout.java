01a3a30d1c3055af35d15e14f69f3d1d1d69bab8
Ryan Bradley <rbradley@redhat.com>
2012-05-28 16:19:42 -0400

    Add more Metawidget support for building and laying out search results.

---
+++ src/main/java/org/jboss/forge/scaffold/spring/metawidget/layout/TableCellLayout.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.forge.scaffold.spring.metawidget.layout;
+
+import java.util.Map;
+
+import org.metawidget.statically.StaticXmlWidget;
+import org.metawidget.statically.html.StaticHtmlMetawidget;
+import org.metawidget.statically.html.layout.HtmlLayout;
+import org.metawidget.statically.html.widgetbuilder.HtmlTableCell;
+import org.metawidget.statically.html.widgetbuilder.HtmlTableHeader;
+
+/**
+ * Layout widgets wrapped in a HtmlTableCell, <td/>, surrounded by an HtmlTableRow, <tr/>
+ * 
+ * @author <a href="mailto:ryan.k.bradley@gmail.com">Ryan Bradley</a>
+ */
+
+public class TableCellLayout
+    extends HtmlLayout
+{
+    @Override
+    public void layoutWidget(StaticXmlWidget widget, String elementName, Map<String, String> attributes, StaticXmlWidget container, StaticHtmlMetawidget metawidget)
+    {
+        if (widget instanceof HtmlTableHeader)
+        {
+            container.getChildren().add(widget);
+            return;
+        }
+
+        HtmlTableCell cell = new HtmlTableCell();
+        cell.getChildren().add(widget);
+        container.getChildren().add(cell);
+    }
+}
---
+++ src/main/java/org/jboss/forge/scaffold/spring/metawidget/widgetbuilder/HeaderWidgetBuilder.java
@@ -0,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.forge.scaffold.spring.metawidget.widgetbuilder;
+
+import static org.metawidget.inspector.InspectionResultConstants.*;
+
+import java.util.Map;
+
+import org.metawidget.statically.StaticXmlMetawidget;
+import org.metawidget.statically.StaticXmlWidget;
+import org.metawidget.statically.html.widgetbuilder.HtmlTableHeader;
+import org.metawidget.widgetbuilder.iface.WidgetBuilder;
+
+public class HeaderWidgetBuilder 
+    implements WidgetBuilder<StaticXmlWidget, StaticXmlMetawidget>
+{
+
+    @Override
+    public StaticXmlWidget buildWidget(String elementName, Map<String, String> attributes, StaticXmlMetawidget metawidget)
+    {
+        if (TRUE.equals(attributes.get(HIDDEN)))
+        {
+            return null;
+        }
+
+        if (ACTION.equals(elementName))
+        {
+            return null;
+        }
+
+        HtmlTableHeader header = new HtmlTableHeader();
+        header.setTextContent(metawidget.getLabelString(attributes));
+
+        return header;
+    }
+}
