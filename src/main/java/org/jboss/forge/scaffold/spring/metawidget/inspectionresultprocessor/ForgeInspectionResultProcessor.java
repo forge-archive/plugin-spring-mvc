1e7c8b14630b53860c79655b9525cd51304f9f6b
Ryan Bradley <rbradley@redhat.com>
2012-07-26 15:59:29 -0400

    Migrate ForgeInspectionResultProcessor to same package as other InspectionResultProcessors

--- src/main/java/org/jboss/forge/scaffold/spring/metawidget/inspectionresultprocessor/ForgeInspectionResultProcessor.java
+++ src/main/java/org/jboss/forge/scaffold/spring/metawidget/inspectionresultprocessor/ForgeInspectionResultProcessor.java
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.jboss.forge.scaffold.spring.metawidget.processor;
+package org.jboss.forge.scaffold.spring.metawidget.inspectionresultprocessor;
 
 import java.util.Map;
 import static org.metawidget.inspector.InspectionResultConstants.*;
